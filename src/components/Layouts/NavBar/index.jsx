45ccce13f621a6ecce8d6d1a0248ff0d18208a45
Đinh Việt Hoàng <viethoanghnid@gmail.com>
2022-08-29 22:58:04 +0700

    fe: add Navbar component

--- src/components/Layouts/NavBar/index.jsx
+++ src/components/Layouts/NavBar/index.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+// import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./style.css";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      <Navbar sticky="top" bg="black" variant="dark">
+        <Navbar.Brand className="brand-name" href="/">
+          <b>LOGO</b>
+        </Navbar.Brand>
+        <Nav className="justify-content-end flex-grow-1 pe-5">
+          <Nav.Item className="nav-align-text">
+            <Nav.Link href="#work">
+              WORK
+              <span className="nav-align-text">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </span>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="nav-align-text">
+            <Nav.Link href="#services">
+              SERVICES
+              <span className="nav-align-text">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </span>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#contact">CONTACT</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar>
+    </div>
+  );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import Intro from "../../components/Intro";
+import NavBar from "../../components/Layouts/NavBar";
+import Work from "../../components/Work";
+import "./style.css";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <NavBar />
+      <section id="home">
+        <Intro />
+      </section>
+      <section id="work">
+        <Work />
+      </section>
     </div>
   );
 }
