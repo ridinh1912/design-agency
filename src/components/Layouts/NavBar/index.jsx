49a9481b5cd8da8935aebaa1786a902650d16f35
Đinh Việt Hoàng <viethoanghnid@gmail.com>
2022-08-30 15:01:31 +0700

    fe: update Navbar

--- src/components/Layouts/NavBar/index.jsx
+++ src/components/Layouts/NavBar/index.jsx
@@ -6,32 +6,44 @@ import "./style.css";
 
 export default function NavBar() {
   return (
-    <div>
-      <Navbar sticky="top" bg="black" variant="dark">
+    <div className="navbar-component">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        sticky="top"
+        bg="black"
+        variant="dark"
+      >
         <Navbar.Brand className="brand-name" href="/">
           <b>LOGO</b>
         </Navbar.Brand>
-        <Nav className="justify-content-end flex-grow-1 pe-5">
-          <Nav.Item className="nav-align-text">
-            <Nav.Link href="#work">
-              WORK
-              <span className="nav-align-text">
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              </span>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="nav-align-text">
-            <Nav.Link href="#services">
-              SERVICES
-              <span className="nav-align-text">
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              </span>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#contact">CONTACT</Nav.Link>
-          </Nav.Item>
-        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className="justify-content-end"
+        >
+          <Nav className="justify-content-end flex-grow-1 pe-5 ml-auto">
+            <Nav.Item className="nav-align-text">
+              <Nav.Link href="#work">
+                WORK
+                <span className="nav-align-text">
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="nav-align-text">
+              <Nav.Link href="#services">
+                SERVICES
+                <span className="nav-align-text">
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#contact">CONTACT</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
